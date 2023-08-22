0d41b0001ba920d989ac6b7677d2f651a2a22842
vijaylaxmi <119170251+Vlaxmi0312@users.noreply.github.com>
2023-08-22 16:34:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,16 @@
-    resource "aws_s3_bucket" "my_bucket" {
-    bucket = "vijaya_tf_bucket"  # Update with your desired bucket name
+    provider "aws" {
+  access_key = "AKIAWZIUDIQS5ZIAEWU7"
+  secret_access_key = "H/QSEyCzJc2WVnBRipgG6LUJNYnfyj4D1F4RuF9F"
+  region = "ap-southeast-2"
+}
 
-    tag = {
-      Name = "vijaya_tf_bucket"
+# Resource Configuration
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "vijaya_s3_bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "MyBucket"
+    Environment = "Development"
   }
 }
-  
