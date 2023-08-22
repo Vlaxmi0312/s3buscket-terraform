0945fbca3486f01e3efdc7d89695450cf26b9e91
vijaylaxmi <119170251+Vlaxmi0312@users.noreply.github.com>
2023-08-22 16:20:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = "ap-southeast-2"  # Update with your desired region
-}
-
     resource "aws_s3_bucket" "my_bucket" {
     bucket = "vijaya_tf_bucket"  # Update with your desired bucket name
 
