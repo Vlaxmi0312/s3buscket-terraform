71db46ea7678e4f82645cc286fbc12ceca102f57
vijaylaxmi <119170251+Vlaxmi0312@users.noreply.github.com>
2023-08-22 15:51:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 provider "aws" {
-  region = "ap-southeast-2"
+  region = "ap-southeast-2"  # Update with your desired region
 }
 
-resource "aws_s3_bucket" "my_vijay_bucket" {
-  bucket = "my-bucket"
+resource "aws_s3_bucket" "vijaya_tf_bucket" {
+  bucket = "your-bucket-name"  # Update with your desired bucket name
+  acl    = "private"           # Set the desired access control list (ACL)
 }
